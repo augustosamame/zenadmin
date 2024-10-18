322fcc6f1f57d68a27890dbe0bbe47a9d2e592a5
Augusto Samame <augustosamame@gmail.com>
2024-10-18 15:05:52 -0500

    dont let edit or delete generic customer

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,7 +4,11 @@ class Admin::OrdersController < Admin::AdminController
   def index
     respond_to do |format|
       format.html do
-        @orders = Order.includes([ :invoices ]).all
+        if current_user.any_admin_or_supervisor?
+          @orders = Order.includes([ :invoices ]).all
+        else
+          @orders = Order.includes([ :invoices ]).where(location_id: @current_location.id)
+        end
         if @orders.size > 500
           @datatable_options = "server_side:true;resource_name:'Order';create_button:false;sort_0_desc;"
         else
