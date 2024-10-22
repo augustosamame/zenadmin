8e8b7ef9294e9031105c98877963166246beb0f6
Augusto Samame <augustosamame@gmail.com>
2024-10-21 22:25:51 -0500

    go back to previous inspect behaviour for activemodel in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.active_record.attributes_for_inspect = :all
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
