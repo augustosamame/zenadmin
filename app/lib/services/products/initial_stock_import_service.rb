4125546ce0a682d8ca77b8a2e32454c8bad60d4a
Augusto Samame <augustosamame@gmail.com>
2024-11-05 17:34:08 -0500

    new scripts to handle transfers from oficina to larcomar

--- app/lib/services/products/initial_stock_import_service.rb
+++ app/lib/services/products/initial_stock_import_service.rb
@@ -73,6 +73,71 @@ module Services
         Product.where(price_cents: 4000).update_all(price_cents: 0, discounted_price_cents: 0)
       end
 
+      def create_missing_products
+        CSV.foreach(@file_path, headers: true).with_index(1) do |row, index|
+          product_name = row[2]
+          found_product = Product.find_by("UPPER(name) = ?", product_name.upcase)
+          if !found_product
+            puts "Product #{product_name} not found"
+            # Product.create!(name: product_name, price_cents: 0, discounted_price_cents: 0)
+          end
+        end
+      end
+
+      def main_warehouse_to_larcomar
+        warehouse = Warehouse.find_by!(name: "Almacén Oficina Principal")
+        user = User.find_by!(email: "almacen_principal@jardindelzen.com")
+        # inventario inicial oficina principal
+        ActiveRecord::Base.transaction do
+          stock_transfer = StockTransfer.create!(
+            user: user,
+            origin_warehouse: nil, # es inventario inicial
+            destination_warehouse: warehouse,
+            transfer_date: Time.current,
+            status: :active,
+            stage: :pending,
+          )
+          CSV.foreach(@file_path, headers: true).with_index(1) do |row, index|
+            product_name = row[2]
+            stock = row[3]&.to_i
+            found_product = Product.find_by("UPPER(name) = ?", product_name.upcase)
+            if found_product && stock.present? && stock > 0
+              StockTransferLine.create!(
+                stock_transfer: stock_transfer,
+                product: found_product,
+                quantity: stock,
+              )
+            end
+          end
+          stock_transfer.finish_transfer!
+        end
+        # transferencia inicial a tiendas
+        warehouse_1 = Warehouse.find_by!(name: "Almacén Larcomar")
+        ActiveRecord::Base.transaction do
+          stock_transfer = StockTransfer.create!(
+            user: user,
+            origin_warehouse: warehouse, # es inventario inicial
+            destination_warehouse: warehouse_1,
+            transfer_date: Time.current,
+            status: :active,
+            stage: :pending,
+          )
+          CSV.foreach(@file_path, headers: true).with_index(1) do |row, index|
+            product_name = row[2]
+            stock = row[3]&.to_i
+            found_product = Product.find_by("UPPER(name) = ?", product_name.upcase)
+            if found_product && stock.present? && stock > 0
+              StockTransferLine.create!(
+                stock_transfer: stock_transfer,
+                product: found_product,
+                quantity: stock,
+              )
+            end
+          end
+          stock_transfer.finish_transfer!
+        end
+      end
+
       def call
         warehouse = Warehouse.find_by!(name: "Almacén Oficina Principal")
         user = User.find_by!(email: "almacen_principal@jardindelzen.com")
