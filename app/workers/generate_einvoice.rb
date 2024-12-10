51f0cbcbe6e985e3e977c1660d7cac0ec33a605b
Augusto Samame <augustosamame@gmail.com>
2024-12-09 19:01:27 -0500

    only retry an einvoice once

--- app/workers/generate_einvoice.rb
+++ app/workers/generate_einvoice.rb
@@ -1,5 +1,6 @@
 class GenerateEinvoice
   include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: 1
 
   def perform(order_id, options = {})
     Rails.logger.info("Inside job: Generating einvoice for order #{order_id}")
