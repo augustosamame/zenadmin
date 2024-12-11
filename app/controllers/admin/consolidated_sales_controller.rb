01b3ffaebc276c2e06b6dc65f22e351f51c70338
Augusto Samame <augustosamame@gmail.com>
2024-12-11 16:53:41 -0500

    improve sunat status and link

--- app/controllers/admin/consolidated_sales_controller.rb
+++ app/controllers/admin/consolidated_sales_controller.rb
@@ -65,8 +65,8 @@ class Admin::ConsolidatedSalesController < Admin::AdminController
           helpers.number_to_currency(record.payment_total.to_f / 100, unit: "S/", format: "%u %n"),
           record.payment_tx,
           record.invoice_custom_id.present? ? helpers.link_to(record.invoice_custom_id, record.invoice_url, target: "_blank") : "",
-          record.invoice_status,
-          helpers.link_to("Ver Detalles", admin_order_path(record.id), class: "btn btn-link")
+          record.invoice_status.present? ? Invoice.sunat_statuses.key(record.invoice_status.to_i)&.humanize : "",
+          helpers.link_to("Ver Detalles", admin_order_path(record.id), class: "text-blue-600 hover:text-blue-800 underline")
         ]
       end
     }
