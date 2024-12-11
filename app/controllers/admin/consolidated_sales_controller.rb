823e699d7caf9395dded2efa1a6dad3a3644bbce
Augusto Samame <augustosamame@gmail.com>
2024-12-11 16:32:55 -0500

    add invoice status and make invoice linkable in consolidated report 2

--- app/controllers/admin/consolidated_sales_controller.rb
+++ app/controllers/admin/consolidated_sales_controller.rb
@@ -64,7 +64,7 @@ class Admin::ConsolidatedSalesController < Admin::AdminController
           record.payment_method,
           helpers.number_to_currency(record.payment_total.to_f / 100, unit: "S/", format: "%u %n"),
           record.payment_tx,
-          record.invoice_custom_id.present? ? helpers.link_to(record.invoice_custom_id, record.invoice_url, target: "_blank") : "",
+          record.invoice_url.present? ? helpers.link_to(record.invoice_custom_id, record.invoice_url, target: "_blank") : "",
           record.invoice_status,
           helpers.link_to("Ver Detalles", admin_order_path(record.id), class: "btn btn-link")
         ]
