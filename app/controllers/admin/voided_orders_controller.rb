6273dc211dbd14c12db7317c33d1d2e0857a2667
Augusto Samame <augustosamame@gmail.com>
2024-12-12 00:38:22 -0500

    add location filter to voided_orders

--- app/controllers/admin/voided_orders_controller.rb
+++ app/controllers/admin/voided_orders_controller.rb
@@ -1,8 +1,15 @@
 class Admin::VoidedOrdersController <  Admin::AdminController
   def index
-    @voided_orders = VoidedOrder.includes(:location, :user)
-                               .order(created_at: :desc)
-                               .page(params[:page])
+    if @current_location
+      @voided_orders = VoidedOrder.includes(:location, :user)
+                                 .where(location_id: @current_location.id)
+                                 .order(created_at: :desc)
+                                 .page(params[:page])
+    else
+      @voided_orders = VoidedOrder.includes(:location, :user)
+                                 .order(created_at: :desc)
+                                 .page(params[:page])
+    end
   end
 
   def show
