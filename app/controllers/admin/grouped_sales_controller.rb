174fb923b6a0f413b8b7d850ec4390070cd731f7
Augusto Samame <augustosamame@gmail.com>
2024-12-24 10:59:07 -0500

    change filter date to local Lima time

--- app/controllers/admin/grouped_sales_controller.rb
+++ app/controllers/admin/grouped_sales_controller.rb
@@ -3,6 +3,10 @@ class Admin::GroupedSalesController < Admin::AdminController
     @start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : Date.current.beginning_of_month
     @end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : Date.current
 
+    # Convert dates to Lima timezone
+    start_datetime = @start_date.in_time_zone("America/Lima").beginning_of_day.utc
+    end_datetime = @end_date.in_time_zone("America/Lima").end_of_day.utc
+
     @selected_location = if current_user.any_admin_or_supervisor?
       location_id = params[:location_id] || session[:location_id]
       Location.find_by(id: location_id)
@@ -10,15 +14,21 @@ class Admin::GroupedSalesController < Admin::AdminController
       @current_location
     end
 
-    base_query = Order.where(order_date: @start_date.beginning_of_day..@end_date.end_of_day)
-                     .group("DATE(order_date)", "locations.id", "locations.name")
+    date_in_lima = "DATE(order_date AT TIME ZONE 'UTC' AT TIME ZONE 'America/Lima')"
+
+    base_query = Order.where(order_date: start_datetime..end_datetime)
+                     .group(Arel.sql(date_in_lima),
+                            "locations.id",
+                            "locations.name")
                      .joins(:location)
-                     .select('DATE(order_date) as sale_date,
-                             locations.id as location_id,
-                             locations.name as location_name,
-                             COUNT(*) as orders_count,
-                             SUM(total_price_cents) as total_sales_cents')
-                     .order("DATE(order_date) DESC, locations.name ASC")
+                     .select(
+                       Arel.sql("#{date_in_lima} as sale_date"),
+                       "locations.id as location_id",
+                       "locations.name as location_name",
+                       "COUNT(*) as orders_count",
+                       "SUM(total_price_cents) as total_sales_cents"
+                     )
+                     .order(Arel.sql("#{date_in_lima} DESC, locations.name ASC"))
 
     @sales_data = if current_user.any_admin_or_supervisor?
       if @selected_location.present?
